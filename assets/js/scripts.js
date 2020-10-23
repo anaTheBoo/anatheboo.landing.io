566319427a00fb158405107bed4cb50de3c92ebb
Ana <analeeskinner@yahoo.com>
2020-10-23 15:24:47 -0500

    init

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.main-content', {
+    direction: 'vertical',
+    slidesPerView: 1,
+    autoHeight: true
+    // mousewheel: true,
+    // pagination: {
+    //     el: '.swiper-pagination',
+    //     clickable: true,
+    // },
+});
